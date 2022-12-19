4126f50e69435da78e952f3de8e43ca9e4e01c0b
Diana16-d <62674835+Diana16-d@users.noreply.github.com>
2022-12-19 06:26:31 -0300

    Add files via upload

---
+++ crud_programacaoweb_2semestre-main/crud_programacaoweb_2semestre-main/Ava_Crud/Crud/form.py
@@ -0,0 +1,19 @@
+from django.forms import ModelForm
+from .models import Aluno, Curso
+from django.forms.widgets import NumberInput
+
+class AlunoForm (ModelForm):
+    
+    class Meta:
+        model = Aluno
+        fields = ('nome','cpf','email','fone','data_nascimento')
+        widgets = {
+            'data_nascimento' : NumberInput(attrs={'type':'date'})
+        }
+
+
+class CursoForm (ModelForm):
+
+    class Meta:
+        model = Curso
+        fields = ('nome', 'requisito', 'carga_horaria', 'preco')
